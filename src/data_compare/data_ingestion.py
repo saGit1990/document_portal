f6f7c56fd4c778d285f116b572f7c399174a58bb
Suel Ahmed <suelahmed@hotmail.com>
2025-08-02 15:10:33 +0530

    code is updated

--- src/data_compare/data_ingestion.py
+++ src/data_compare/data_ingestion.py
@@ -1,6 +1,21 @@
-import os
+import sys
 import fitz
-import uuid
+from pathlib import Path
 from datetime import datetime
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import CustomDocumentException
+
+
+class DocumentComparor:
+    def __init__(self):
+        pass 
+    
+    
+    def delete_existing_file(self, file_path):
+        str 
+        
+    def save_uploaded_files(self):
+        pass 
+    
+    def read_pdf(self):
+        pass 
